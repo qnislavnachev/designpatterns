ef1bfd94f59aa336fd7a5452bbef394882d3103f
Vasil <v.mitov.clouway@gmail.com>
2016-08-26 09:15:56 +0300

    Singleton simplified.

--- src/com/clouway/Demo.java
+++ src/com/clouway/Demo.java
@@ -5,14 +5,7 @@ package com.clouway;
  */
 public class Demo {
   public static void main(String[] args) {
-    Host host=new Host();
-    Organiser organiser=new Organiser();
-    System.out.println(host.invite("Denis"));
-    System.out.println(host.invite("Vasko"));
-    System.out.println(host.invite("Marto"));
-    System.out.println(host.invite("Qni"));
-    System.out.println(host.uninvite("Qni"));
-    System.out.println(organiser.sendInvites());
+    Singleton singleton = Singleton.getInstance();
+    Singleton singleton1= Singleton.getInstance();
   }
-
 }
