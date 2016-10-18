6d03505ed7df88f35ddd9b109f24627d9e60ddfd
qnislavnachgev <qnislav.nachev@gmail.com>
2016-10-18 12:50:35 +0300

    Changed name of test

--- test/tests/SingletonTest.java
+++ test/tests/SingletonTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class SingletonTest {
 
     @Test
-    public void sameTarget() throws Exception {
+    public void twoObjectsAreSame() throws Exception {
         Singleton obj1 = Singleton.getObject();
         Singleton obj2 = Singleton.getObject();
         assertThat(obj1, is(obj2));
