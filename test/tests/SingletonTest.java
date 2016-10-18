50857dd65c50ac55b790123fb8ea4107968eb325
qnislavnachgev <qnislav.nachev@gmail.com>
2016-10-18 12:46:37 +0300

    Added test

---
+++ test/tests/SingletonTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+import task2.Singleton;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    public void sameTarget() throws Exception {
+        Singleton obj1 = Singleton.getObject();
+        Singleton obj2 = Singleton.getObject();
+        assertThat(obj1, is(obj2));
+    }
+}
